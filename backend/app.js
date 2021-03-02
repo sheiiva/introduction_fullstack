048547b56a855fdff35d696181612c9dc8e48096
Corentin ROZET <corentin.rozet@epitech.eu>
2021-03-02 17:27:22 +0100

    [ADD] delete call

--- backend/app.js
+++ backend/app.js
@@ -41,9 +41,15 @@ app.post('/api/stuff', (req, res, next) => {
 
 app.put('/api/stuff/:id', (req, res, next) => {
     Thing.updateOne({ _id: req.params.id}, { ...req.body, _id: req.params.id })
-        .then(thing => res.status(200).json( { message: 'Object modified!' }))
+        .then(() => res.status(200).json( { message: 'Object modified!' }))
         .catch(err => res.status(404).json({ err }));
-    });
+});
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Object deleted!'}))
+        .catch(err => res.status(400).json({ err }));
+});
 
 app.get('/api/stuff/:id', (req, res, next) => {
     Thing.findOne({ _id: req.params.id })
