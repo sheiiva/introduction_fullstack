88e964fdca2f048bf16d418f20aed3abe8c6b6d2
Corentin ROZET <corentin.rozet@epitech.eu>
2021-03-02 16:24:56 +0100

    [ADD] db model

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const Thing = require('./models/thing');
+const thing = require('./models/thing');
 
 mongoose.connect(
     'mongodb://' + process.env.MONGODB_IP + ':' + process.env.MONGODB_PORT + '/backend',
@@ -40,25 +41,9 @@ app.post('/api/stuff', (req, res, next) => {
 });
 
 app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
+    Thing.find()
+        .then(things => res.status(200).json(things))
+        .catch(err => res.status(400).json({ error: err }));
 });
 
 module.exports = app;
