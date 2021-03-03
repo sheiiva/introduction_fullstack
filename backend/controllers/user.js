8d1e824dad926fbf6f41739fee487aa69d445140
Corentin ROZET <corentin.rozet@epitech.eu>
2021-03-03 11:45:05 +0100

    [ADD] singup logic logic

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -18,5 +18,22 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ error: 'User not found.'});
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ error: 'Password incorrect.'});
+                    }
+                    return res.status(200).json({
+                        userId: user._id,
+                        token: 'TOKEN'
+                    });
+                })
+                .catch(err => res.status(500).json({ err }));
+        })
+        .catch(err => res.status(500).json({ err }));
 };
