87a9c9550ac956152499be95edb4ce8f1a5834bc
Corentin ROZET <corentin.rozet@epitech.eu>
2021-03-03 15:38:20 +0100

    [UPDATE] token: implemented authorization from token

---
+++ backend/middleware/auth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const userId = decodedToken.userId;
+
+        if (req.body.userId && req.body.userId !== userId) {
+            throw 'Wrong user ID.';
+        } else {
+            next();
+        }
+    } catch (error) {
+        res.status(401).json({ error: error | 'Request no authentified.' });
+    }
+};
